afb20a58f9f08ef21e55a4c4bcc419be11e0c3cd
Raghavendra17N <raghava01995@gamil.com>
2025-03-24 13:10:40 +0530

    updates mongodb config

--- config/db.js
+++ config/db.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect('mongodb://localhost:27017/myApp', {
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/myApp';
+
+mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
+
+  module.exports = mongoose;
