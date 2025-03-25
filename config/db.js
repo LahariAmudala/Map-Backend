6abad0ef288f0031cc6c04ba9d3d37c3507b3f9a
Raghavendra17N <raghava01995@gamil.com>
2025-03-25 10:06:19 +0530

    updated user testing script

--- config/db.js
+++ config/db.js
@@ -1,3 +1,17 @@
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+
+// const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/myApp';
+
+// mongoose.connect(MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+//   .then(() => console.log('MongoDB connected'))
+//   .catch(err => console.error('MongoDB connection error:', err));
+
+//   module.exports = mongoose;
+
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -7,7 +21,7 @@ mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .then(() => console.log('✅ MongoDB connected successfully'))
+  .catch(err => console.error('❌ MongoDB connection error:', err));
 
-  module.exports = mongoose;
+module.exports = mongoose;
